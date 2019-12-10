6698b571ebf55428bb1d2a7b1917662c19fbf969
Sebastien Hamel <sebastien.hamel@gmail.com>
2019-12-10 11:35:19 +0700

    first commit

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,8 +2,18 @@ import 'package:angular/angular.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: ''''
+    <h1>Hello {{name}}</h1>
+    <p>
+      This is some text to test that Angular Dart get indexed by Google search bot and 
+      therefore has some good SEO. This code was published on December 10, 2019.   
+      The Angular Dart package is not indexed in the first pass, this we know, but
+      what we really want to know is how it will take for it to be indexed by Google.  
+      We don't care about the rank in the search results for now, we only want to be 
+      sure it will be indexed eventually.
+    </p>
+  ''',
 )
 class AppComponent {
-  var name = 'Angular';
+  var name = 'Sébastien';
 }
